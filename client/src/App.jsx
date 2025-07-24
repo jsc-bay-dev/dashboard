3d3c7bec2a048bb231c41420b103dc5247d0cee9
Justin Choi <j@Justins-MBP.lan>
2025-07-23 21:06:19 -0700

    calculator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
-import Clock from './Widgets/Clock'
+import Clock from './widgets/Clock'
+import Calculator from './widgets/Calculator'
+import SoundCloud from './widgets/Soundcloud'
+
+const importHandler = () => {
+  return (
+   <div className="widget"></div> 
+  )
+}
 
 function App() {
 
   return (
     <div className="app">
+      <Calculator/>
       <Clock/>
+      <SoundCloud/>
       <div className="add_widgets">
         {/* bottom left of window. click for list. */}
       </div>
